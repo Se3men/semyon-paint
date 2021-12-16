af13988f51dc0f21df54c8e8b7d91e61587e1ffe
SemyonSergeev <semyonsergeev30@gmail.com>
2021-12-16 20:58:14 +0300

    Release dotenv

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const WSserver = require('express-ws')(app);
@@ -6,7 +7,7 @@ const cors = require('cors');
 const fs = require('fs').promises;
 const path = require('path');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ?? 5000;
 
 app.use(cors());
 app.use(express.json());
