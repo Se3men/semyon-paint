768df4cbcb8a6a50eefb1c3a968e10e8e69daffa
SemyonSergeev <semyonsergeev30@gmail.com>
2021-12-18 11:48:09 +0300

    root /

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/index', (req, res) => {
+app.get('/', (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
   return res.render('index');
 });
